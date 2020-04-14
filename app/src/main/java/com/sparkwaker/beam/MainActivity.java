306fef3c2ab55300cb69e4bfbf8602e9b6b75e51
Michel Palma <alejandropalm28@gmail.com>
2020-04-14 10:02:00 -0500

    - Se agregan colores   - Se realiza layout Item Sound  -Se establece theme por defecto   - Se agregan algunos layouts   - Se agregan algunos iconos - Se agregan algunos strings

--- app/src/main/java/com/sparkwaker/beam/MainActivity.java
+++ app/src/main/java/com/sparkwaker/beam/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
+       // getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
